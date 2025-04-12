071f40ad586c92c322503a0c5e180b8d66e9e895
Abdelwahab <ab28fb@gmail.com>
2025-04-12 05:35:05 +0100

    todo in cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:5173', // Your React app's origin
+    origin: [
+      'http://localhost:5173', // Your React app's origin
+      // todo: add production domain
+      'https://your-production-domain.com', // Production domain
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     // credentials: true, // If you need to pass cookies
