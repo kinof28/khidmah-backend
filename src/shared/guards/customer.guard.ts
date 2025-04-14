f7560c18b0982caa09a260b6589b02a47981cd91
Abdelwahab <ab28fb@gmail.com>
2025-04-14 13:46:16 +0100

    Fix: remove comments

--- src/shared/guards/customer.guard.ts
+++ src/shared/guards/customer.guard.ts
@@ -1,12 +1,9 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class CustomerGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
-    // Todo: remove console.log
-    // console.log('request.user: ', request.payload);
     return request.payload.role === 'customer';
   }
 }
